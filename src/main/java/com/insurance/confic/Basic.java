1bd922f5a0074ca71b4082a9293f5f7854107017
karthikeyan799 <akarthikeyan799@gmail.com>
2025-11-08 10:06:33 +0530

    change password

--- src/main/java/com/insurance/confic/Basic.java
+++ src/main/java/com/insurance/confic/Basic.java
@@ -9,7 +9,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-
 @Configuration
 public class Basic {
 //	package com.demo.config;
@@ -22,30 +21,30 @@ public class Basic {
 //	@Configuration
 //	public class CorsConfig {
 
-	
-	    @Bean
-	    public CorsFilter corsFilter() {
-	        // Create a configuration source and a CORS configuration
-	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	        CorsConfiguration config = new CorsConfiguration();
+	@Bean
+	public CorsFilter corsFilter() {
+		// Create a configuration source and a CORS configuration
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
 
 //	         Allow requests from 'http://localhost:3000' (your frontend URL)
-	        config.addAllowedOrigin( "http://localhost:3000");
-	        config.addAllowedOrigin("http://localhost:5173");
-	        config.addAllowedOrigin("https://oldinsuranceproject.netlify.app/");
+		config.addAllowedOrigin("http://localhost:3000");
+		config.addAllowedOrigin("http://localhost:5173");
+		config.addAllowedOrigin("https://oldinsuranceproject.netlify.app/");
 
-	        
-	        // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-	        config.addAllowedMethod("*");
+		config.addAllowedOrigin("https://newinsuranceproject.netlify.app/");
 
-	        // Allow all headers in the request
-	        config.addAllowedHeader("*");
+		// Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+		config.addAllowedMethod("*");
 
-	        // Register the CORS configuration for all endpoints (/**)
-	        source.registerCorsConfiguration("/**", config);
+		// Allow all headers in the request
+		config.addAllowedHeader("*");
 
-	        // Create and return a CorsFilter with the configured settings
-	        return new CorsFilter(source);
-	    }
-	
+		// Register the CORS configuration for all endpoints (/**)
+		source.registerCorsConfiguration("/**", config);
+
+		// Create and return a CorsFilter with the configured settings
+		return new CorsFilter(source);
+	}
+
 }
